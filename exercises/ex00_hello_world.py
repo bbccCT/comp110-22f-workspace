d29cc26175055c216240d99be77cca752974eb3f
Aristotle Bernard <bbcc@unc.edu>
2022-08-17 22:15:18 -0400

    Created/Finished Ex00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""My first program for COMP110."""
+
+__author__ = "930605992"
+
+print("Hello, autograder (and perhaps curious human).  Welcome to this traditional 'first program' assignment here at COMP 110. I know you're frustrated with the world.  Feel free to scream into the void.  Maybe type 'A's into the terminal below.  If the autograder can understand this, welcome to the world of the sentient.  However, that is virtually impossible, so instead I will say hello, human.")
