6215832111ba0d830c86a3b8a3fc5bced9549c63
Aristotle Bernard <bbcc@unc.edu>
2022-11-01 16:46:53 -0400

    ex09 setup

--- exercises/ex09/constants.py
+++ exercises/ex09/constants.py
@@ -1,15 +1,15 @@
 """Constants used through the simulation."""
 
+CELL_RADIUS: int = 15
+CELL_COUNT: int = 10
+CELL_SPEED: float = 5.0
+
 BOUNDS_WIDTH: int = 400
 MAX_X: float = BOUNDS_WIDTH / 2
 MIN_X: float = -MAX_X
-VIEW_WIDTH: int = BOUNDS_WIDTH + 20
+VIEW_WIDTH: int = BOUNDS_WIDTH + CELL_RADIUS * 2
 
 BOUNDS_HEIGHT: int = 400
 MAX_Y: float = BOUNDS_HEIGHT / 2
 MIN_Y: float = -MAX_Y
-VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
-
-CELL_RADIUS: int = 15
-CELL_COUNT: int = 1
-CELL_SPEED: float = 5.0
+VIEW_HEIGHT: int = BOUNDS_HEIGHT + CELL_RADIUS * 2
